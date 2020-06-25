7ec099ed6c6115dd98bdddd3c45efd52c77840ee
timur612 <tz220404@gmail.com>
2020-06-25 13:32:48 +0900

    але

--- src/views/Question.vue
+++ src/views/Question.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <h2> {{questionName[number_id]}} </h2>
         <button @click="goToTheNextQuestion" > далее - {{number_id}} </button>
         <button @click="backToTheQustion"> назад </button>
         <p> {{questionNumber}} </p>
@@ -16,28 +17,47 @@ export default {
         return{
             number_id: 0,
             questId: 0,
-            questionNumber: []
+            questionNumber: [],
+            questionName: [],
         }
     },
-    mounted(){
+    created(){
         this.number_id = this.$route.query.numberId
         this.questId = this.$route.query.questId;
-         axios.post('http://localhost/quedu_server/question.php',this.questId)
+        axios.post('http://localhost/quedu_server/question.php',this.questId)
                     .then(response =>{
                         //console.log(response.data.Question.length);
                         for(let i=0;i<response.data.Question.length;i++){
                             this.questionNumber.push(response.data.Question_numbers[i][0])
+                            this.questionName.push(response.data.Question[i][0])
                         }
                     })
                     .catch(function (error) {
                         console.log(error);
                     });
     },
+    mounted(){
+        let param = {
+                    number_id: this.number_id,
+                    questId: this.questId,
+            };
+        const str = JSON.stringify(param);
+
+        axios.post('http://localhost/quedu_server/PUTHERESCRIPT.php',str)
+                .then(response=>{
+                    console.log(response.data)
+                })
+                .catch(e=>{
+                    console.log(e)
+                })
+    },
     methods:{
         goToTheNextQuestion(){
-           for(let i=0;i<this.questionNumber.length;i++){
-               if(this.number_id != this.questionNumber[i]){
-                   router.push({ path: '/question',query:{numberId: this.questionNumber[i],questId:this.questId} })
+            let id = this.number_id+1;
+            console.log (this.number_id);
+            for(let i=0;i<this.questionNumber.length;i++){
+               if(id == this.questionNumber[i]){
+                   router.push({ path: '/question',query:{numberId: id,questId:this.questId} })
                    this.number_id = this.$route.query.numberId
                }
            }
